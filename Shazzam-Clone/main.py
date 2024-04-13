e507db356e3aa621ac55d328123e2da6e2cba66d
wapitil <wapiti@gmail.com>
2024-04-13 16:57:53 +0000

    增加了通过AudioFingerPrint技术识别音乐的程序，可能会代替现有的音乐识别程序

---
+++ Shazzam-Clone/main.py
@@ -0,0 +1,19 @@
+from logic.Shazzam import Shazzam
+from mic import record_audio
+
+if __name__=="__main__":
+    engine = Shazzam()
+    '''
+    this will add songs present in 'mp3' folder to database
+    don't worry if song is already added, it wont over-write so saves time :)
+    '''
+    engine.add_songs()
+    '''
+    just write song name, which is present in 'test' folder 
+    no need to mention relative path
+    '''
+    # engine.match_song('5.m4a')
+    filename = "output"
+    record_audio(f"./test/{filename}")
+    engine.match_song(f"{filename}.mp3")
+
